a704d6c16aa95285da616e25c507743ff6d65f02
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-06-17 18:08:04 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,9 @@
 def turn_count(board)
    counter = 0
   board.each do |token|
- if token == "O" || token == "X"
+ if token == "O" 
+  counter += 1
+elsif token == "X"
   counter += 1
    return counter
   end
